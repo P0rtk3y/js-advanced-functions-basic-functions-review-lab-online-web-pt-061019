f3e96ac8a59002b2e5022c9a9de25190d2fdc05a
Lana Worobec <llama.p0rtk3y@gmail.com>
2020-02-01 03:52:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function saturdayFun(fun = 'roller-skate'){
+  return `This Saturday, I want to ${fun}!`;
+}
+
+
+let mondayWork = function(work = "go to the office"){
+  return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(visualFlair="*"){
+ return function (adj="special"){
+   return `You are ${visualFlair}${adj}${visualFlair}!`;
+ }
+}
+
+const Calculator = {
+  add: (a,b) => a + b,
+  subtract: (a,b) => a - b,
+  multiply: (a,b) => a * b,
+  divide: (a,b) => a / b
+}
+
